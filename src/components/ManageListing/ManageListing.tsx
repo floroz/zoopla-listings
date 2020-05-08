762f3d09c933e6886d0469665ed3e171e45ead22
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-05-08 10:49:49 +0100

    abstract async logic

--- src/components/ManageListing/ManageListing.tsx
+++ src/components/ManageListing/ManageListing.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { usePropertyListing } from "../../hooks/usePropertyListing";
+import usePropertyListing from "../../hooks/usePropertyListing";
+import { IPropertyListing } from "../../interfaces";
+import Listing from "../Listing/Listing";
+
 interface Props {}
 
 const ManageListing = (props: Props) => {
-  const { properties, setProperties } = usePropertyListing();
+  const { properties, retryFetch, loading } = usePropertyListing();
 
-  return <div>ManageListing</div>;
+  return (
+    <section>
+      <h1>ManageListing</h1>
+      <div>
+        {!loading &&
+          properties?.length > 0 &&
+          properties.map((property: IPropertyListing) => <Listing />)}
+      </div>
+    </section>
+  );
 };
 
 export default ManageListing;
--- src/hooks/usePropertyListing.ts
+++ src/hooks/usePropertyListing.ts
@@ -1,23 +1,88 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useCallback } from "react";
 import { IPropertyListing } from "../interfaces/index";
 
 const API_URL: string = process.env.REACT_APP_API || "http://localhost:4000";
 const API_URL_PROPERTIES: string = `${API_URL}/properties`;
 
-export const usePropertyListing = () => {
-  const [properties, setProperties] = useState<null | IPropertyListing[]>(null);
+enum FetchProperties {
+  SUCCESS = "SUCCESS",
+  START = "START",
+  ERROR = "ERROR",
+}
 
-  useEffect(() => {
-    async function fetchProperties(propertiesUrl: string) {
-      try {
-        let res = await fetch(propertiesUrl);
-        let json = await res.json();
-        console.log(json);
-      } catch (error) {}
+interface IAction {
+  type: FetchProperties;
+  payload?: any;
+}
+
+interface IPropertyListingState {
+  properties: null | IPropertyListing[];
+  loading: boolean;
+  error: null | string;
+}
+
+const initialState: IPropertyListingState = {
+  properties: null,
+  loading: false,
+  error: null,
+};
+
+function reducer(state: IPropertyListingState = initialState, action: IAction) {
+  switch (action.type) {
+    case FetchProperties.START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FetchProperties.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        properties: action.payload,
+      };
+    case FetchProperties.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+const usePropertyListing = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchProperties = useCallback(async function () {
+    try {
+      dispatch({
+        type: FetchProperties.START,
+      });
+      let res = await fetch(API_URL_PROPERTIES);
+      let json = await res.json();
+      dispatch({
+        type: FetchProperties.SUCCESS,
+        payload: json,
+      });
+    } catch (error) {
+      dispatch({
+        type: FetchProperties.ERROR,
+        payload: error.message,
+      });
+      console.error(error.message);
     }
-
-    fetchProperties(API_URL_PROPERTIES).then();
   }, []);
 
-  return { properties, setProperties };
+  useEffect(() => {
+    fetchProperties().then();
+  }, [fetchProperties]);
+
+  return {
+    properties: state.properties,
+    retryFetch: fetchProperties,
+    loading: state.loading,
+  };
 };
+
+export default usePropertyListing;
