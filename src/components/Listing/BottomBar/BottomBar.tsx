2736490ee651edf6890565610df8934b0e0695ab
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-05-09 15:58:03 +0100

    date formatting

--- src/components/Listing/BottomBar/BottomBar.tsx
+++ src/components/Listing/BottomBar/BottomBar.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { ReactComponent as StarSVG } from "../../../icons/star.svg";
 import styles from "./BottomBar.module.scss";
+import { formatDate } from "../../../utils";
 
 interface Props {
   listed: string;
 }
 
 const BottomBar = (props: Props) => {
-  let date = new Date(props.listed).toLocaleDateString();
-
   return (
     <div className={styles.bottomBar}>
       <div className={styles.date}>
-        <span>Listed on: {date}</span>
+        <span>Listed on: {formatDate(props.listed)}</span>
       </div>
       <div className={styles.save}>
         <StarSVG className={styles.starSVG} />
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,10 @@
+import moment from "moment";
+
 export function formatPrice(price: number) {
   let formatter = new Intl.NumberFormat();
   return formatter.format(price);
+}
+
+export function formatDate(listingDate: string): string {
+  return moment(listingDate).format("Do MMM YYYY");
 }
