0c12434c60b573b3ee4b070982794c88e6385e5e
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-05-09 16:27:31 +0100

    fixed 'act' warning

--- src/components/ManageListing/ManageListing.test.tsx
+++ src/components/ManageListing/ManageListing.test.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ManageListings from "./ManageListing";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
-import { render } from "@testing-library/react";
+import { render, RenderResult, wait } from "@testing-library/react";
 import fetchMock, { enableFetchMocks } from "jest-fetch-mock";
 import jsonResponse from "../../../db.json";
-import { act } from "react-dom/test-utils";
+
 enableFetchMocks();
 
-let renderFn = (component, options = {}) => {
+let renderFn = (component: React.ReactElement, options = {}): RenderResult => {
   let history = createMemoryHistory();
 
   return render(<Router history={history}>{component}</Router>, { ...options });
@@ -19,18 +19,22 @@ describe("<ManageListings />", () => {
     fetchMock.resetMocks();
   });
 
-  test("should render without error ", () => {
+  test("should render without error ", async () => {
     fetchMock.mockResponse(() => Promise.resolve(JSON.stringify(jsonResponse)));
 
-    let { getByTestId } = renderFn(<ManageListings />);
+    let wrapper: RenderResult = renderFn(<ManageListings />);
 
-    expect(getByTestId(/manage-listing/i)).toBeDefined();
+    await wait();
+
+    expect(wrapper.getByTestId(/manage-listing/i)).toBeDefined();
   });
 
   test("should correctly call the api once to fetch the properties", async () => {
     fetchMock.mockResponse(() => Promise.resolve(JSON.stringify(jsonResponse)));
 
-    await act(async () => renderFn(<ManageListings />));
+    renderFn(<ManageListings />);
+
+    await wait();
 
     expect(fetchMock).toHaveBeenCalled();
     expect(fetchMock).toHaveBeenCalledTimes(1);
