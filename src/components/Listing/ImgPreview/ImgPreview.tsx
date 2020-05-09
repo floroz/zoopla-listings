4a1bd49d0f655352f07207c6378709edb25a192f
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-05-09 10:10:27 +0100

    a11y checks

--- src/components/Listing/ImgPreview/ImgPreview.tsx
+++ src/components/Listing/ImgPreview/ImgPreview.tsx
@@ -9,18 +9,18 @@ interface Props {
 
 const ImgPreview = ({ images = [], expired = false }: Props) => {
   return (
-    <div className={styles.container}>
+    <figure className={styles.container} aria-label="house preview">
       {expired && (
         <div className={styles.expired}>
           <span>Expired</span>
         </div>
       )}
-      <img src={images[0]} alt="property" className={styles.img} />
+      <img src={images[0]} alt="rooms and overview" className={styles.img} />
       <div className={styles.camera}>
         <CameraSVG className={styles.svg} />
         <span>{images.length && images.length}</span>
       </div>
-    </div>
+    </figure>
   );
 };
 
